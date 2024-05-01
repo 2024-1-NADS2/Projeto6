342015ac3d6fe6510acc61282b67e83f782ae3d7
filipipires12 <149540785+filipipires12@users.noreply.github.com>
2024-05-01 00:06:52 -0300

    Add files via upload

---
+++ pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeaderLogin from '../components/HeaderLogin'
+import CaixaLogin from '../components/CaixaLogin';
+
+function Login() {
+    return (
+        <div>
+            <HeaderLogin />
+            <CaixaLogin />
+        </div>
+    );
+}
+export default Login;
