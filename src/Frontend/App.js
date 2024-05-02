605b767dbd8ea5dbb2966dce0081b7ba9aee2575
filipipires12 <149540785+filipipires12@users.noreply.github.com>
2024-05-02 13:02:47 -0300

    Update App.js

--- src/Frontend/App.js
+++ src/Frontend/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Perfil from './pages/Perfil';
 import Feed from './pages/Feed';
 import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+import CadastroOng from './pages/CadastroOng';
 // import GlobalStyles from './styles/GlobalStyle';
 
-function App(){
-    return(
+function App() {
+    return (
         <Router>
             <Routes>
-                <Route path="/" element={<Perfil />}/>
-                <Route path="/feed" element={<Feed />}/>
-                <Route path="/login" element={<Login />}/>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Perfil />} />
+                <Route path="/feed" element={<Feed />} />
+                <Route path="/cadastro" element={<Cadastro />}/>
+                <Route path="/cadastroOng" element={<CadastroOng />}/>
             </Routes>
         </Router>
     );
