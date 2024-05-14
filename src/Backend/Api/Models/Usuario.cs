15a90eae367bc515539db8585b23bc50a3c50d25
filipipires12 <149540785+filipipires12@users.noreply.github.com>
2024-05-14 00:13:13 -0300

    Add files via upload

---
+++ src/Backend/Api/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServidorExemplo.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int id_usuario { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string nome { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string senha_hash { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string email { get; set; }
+    }
+
+}
