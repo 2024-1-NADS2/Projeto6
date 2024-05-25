86b1eb64a36fe50e1301aaf973fbfd33fbf6bda1
filipipires12 <149540785+filipipires12@users.noreply.github.com>
2024-05-25 07:46:20 -0300

    Add files via upload

---
+++ Models/Ong.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication3.Models;
+
+namespace WebApplication2.Models
+{
+    public class Ong
+    {
+        [Key]
+        public int id_ong { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string nome { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string senha_hash { get; set; }
+
+        [Required]
+        [MaxLength(14)]
+        public string cnpj { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string email { get; set; }
+
+      //  public virtual ICollection<Postagem> Postagens { get; set; }
+    }
+}
